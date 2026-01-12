bae5a28d2d75469e6f4e1ba5ca6f98da1d06daca
PandaDasKissen <55free77@gmail.com>
2026-01-12 16:29:48 -0700

    formatting gdict output

--- freya/gdict/dict.js
+++ freya/gdict/dict.js
@@ -3,29 +3,60 @@ const page = "Sheet1"
 const data = await fetch("https://opensheet.elk.sh/"+url+"/"+page)
   .then(r => r.json());
 console.log(data);
+data.forEach(word => {
+    word.Translation = word.Translation.split(",").map(thing => thing.trim())
+})
 
 var Eng2Ger = false
 
+function edit(word,fn,arg) {
+    return word.Translation.map(thing => thing[fn](arg))
+}
+
 const searchInput = document.getElementById("search");
 searchInput.addEventListener("input", onSearchInput);
-function render(greg) {
+function render(words) {
     result.innerHTML = ""
-    greg.forEach(item => {
-        const peter = document.createElement("div")
-        peter.textContent = Eng2Ger?item.Word:item.Translation
-        result.append(peter)
+    words.forEach(word => {
+        const wrap = document.createElement("div")
+        wrap.className = "wrap"
+
+        const title = document.createElement("h2")
+        title.className = "wordTitle" 
+        title.textContent = word.Word
+
+        const trans = document.createElement("p")
+        trans.className = "trans"
+        trans.textContent = "English:"+word.Translation.join(", ")+"\n\n"+word.Explanation
+
+        
+
+        
+
+
+        wrap.append(title,trans)
+        result.append(wrap)
     });
 }
+
 function onSearchInput() {
     const query = searchInput.value.toLowerCase();
     if (query == "")
         return
     var filtered
     if (Eng2Ger == true)
-        filtered = data.filter(item => item.Translation.toLowerCase().includes(query));
+        filtered = data.filter(word => {
+            var found = false
+            const tempList = edit(word,"toLowerCase")
+            tempList.forEach(thing => {
+                if (thing.startsWith(query))
+                    found = true
+            })
+            return found
+        });
 
     else
-        filtered = data.filter(item => item.Word.toLowerCase().startsWith(query));
+        filtered = data.filter(word => word.Word.toLowerCase().startsWith(query));
     console.log(filtered);
     render(filtered)
 }
